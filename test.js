5ee78650f13b9b12c873355543ee6398f3957193
Nit <66027553+Nitlix@users.noreply.github.com>
2023-06-28 20:17:51 +0300

    Array and Hashmap Cycles + Repeats
    + lixtools/dict/cycle
    + lixtools/list/cycle
    + lixtools/repeat

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-import {
-    benchmark,
-    repeatTimes, unix
-} from "./src/index.js";
+// import {
+//     benchmark,
+//     repeatTimes, unix
+// } from "./src/index.js";
 
-import fs from "fs"
+// import fs from "fs"
 
-console.log(benchmark(async ()=> {
-    const content = await fs.readFileSync("C:\\Users\\pravi\\Desktop\\lixbase.png").toString()
-    console.log(content)
-}))
+// console.log(benchmark(async ()=> {
+//     const content = await fs.readFileSync("C:\\Users\\pravi\\Desktop\\lixbase.png").toString()
+//     console.log(content)
+// }))
+
+const list_cycle = require("./src/list_cycle.js")
+
+list_cycle()
